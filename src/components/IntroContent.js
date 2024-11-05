724fd97662b44e2ded0f9d09e595c9754418bcc5
Edward Yuabov <yuaboved@gmail.com>
2024-11-05 09:18:05 -0500

    added latest work snapshots

--- src/components/IntroContent.js
+++ src/components/IntroContent.js
@@ -1,15 +1,18 @@
 import "./IntroContent.css"
 import Loading from "./Loading"
+import CarvoySS from "../images/CarvoyAppScreenshot.jpg"; 
+import OneUpWebsiteSS from "../images/1upWebsiteScreenshot.png"; 
+import GeneralAssemblySS from "../images/GeneralAssemblyScreenshot.png"; 
 
 const IntroContent = () => {
 
     return(
         <div className="intro_content">
             <div className="top_content">
-                <Loading/>
+                {/* <Loading/> */}
                 <div className="title">
                     <span className="main">EDWARD YUABOV</span>
-                    <span className="description">FRONTEND ENGINEER</span>
+                    <span className="description">WEB DEVELOPER & UX DESIGNER</span>
                 </div>
             </div>
             <div className="bottom_content">
@@ -17,9 +20,15 @@ const IntroContent = () => {
                     <span>Some of my latest work</span>
                 </div>
                 <div className="project_container">
-                    <div className="project"></div>
-                    <div className="project"></div>
-                    <div className="project"></div>
+                    <div className="project">
+                        <img src={CarvoySS}/>
+                    </div>
+                    <div className="project">
+                        <img src={OneUpWebsiteSS}/>
+                    </div>
+                    <div className="project">
+                    <img src={GeneralAssemblySS}/>
+                    </div>
                 </div>
             </div>
         </div>
