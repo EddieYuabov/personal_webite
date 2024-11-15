ce55e9f30ff0b84ab49add120efe229e9be88125
Edward Yuabov <yuaboved@gmail.com>
2024-11-15 13:37:32 -0500

    added about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -22,6 +22,43 @@ const About = () => {
     
         return () => clearTimeout(timer);
     }, []);
+
+    const experience = [
+        {
+            year: "2023 - present",
+            title: "Full-stack web & app developer",
+            company: "1up"
+        }, 
+        {
+            year: "2022 - 2023",
+            title: "Front-end UI designer & developer",
+            company: "General Assembly"
+        },
+        {
+            year: "2021 - 2022",
+            title: "UI developer & algorithm designer",
+            company: "Carvoy"
+        }
+    ]
+
+    const education = [
+        {
+            year: "2019 - 2023",
+            title: "Bachelor of Computer Science",
+            company: "Queens College"
+        },
+        {
+            year: "2022 - present",
+            title: "Software Engineering course",
+            company: "Harvard University"
+        }, 
+        {
+            year: "2021 - 2022",
+            title: "Full-Stack Software immersive course",
+            company: "General Assembly"
+        }
+    ]
+
     return(
     <div className="about">
         {loading ? <Loading/> : null}
@@ -31,7 +68,32 @@ const About = () => {
         <p>learn a little about me</p>
     </div>
         <div className="content">
-            
+            <div className="experience">
+            <div className="text">
+                <span>experience.</span>
+            </div>
+                {experience.map((item, index) => (
+                    <div key={index} className="cell">
+                    <p>{item.year} </p>
+                        <h3>{item.title}</h3>
+                        <p>{item.company}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="education">
+            <div className="experience">
+            <div className="text">
+                <span>education.</span>
+            </div>
+                {education.map((item, index) => (
+                    <div key={index} className="cell">
+                        <p>{item.year} </p>
+                        <h3>{item.title}</h3>
+                        <p>{item.company}</p>
+                    </div>
+                ))}
+            </div>
+            </div>
         </div>
     </div>
     )
